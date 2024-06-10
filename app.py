b659bd799350e11b78cfe31cd1eab0bfe51fc4e8
Ali <nic.oleda.l.i.rae@gmail.com>
2024-06-10 19:13:40 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def driversetup():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # Run Selenium in headless mode
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("lang=en")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--incognito")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(options=options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined});")
+    return driver
+    
 app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
+  driver = driversetup()
+  
+  return driver.cur + 'Hello from Koyeb'
  
  
 if __name__ == "__main__":
