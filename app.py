8538e26eedfc84e316d7ecb251d8e68d685b9be6
Ali <nic.oleda.l.i.rae@gmail.com>
2024-06-10 19:54:42 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+'''from flask import Flask
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -30,4 +30,47 @@ def hello_world():
  
  
 if __name__ == "__main__":
+    app.run()'''
+from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+
+app = Flask(__name__)
+
+def driversetup():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # Run Selenium in headless mode
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("lang=en")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--incognito")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    
+    service = ChromeService(executable_path='/path/to/chromedriver')  # Specify the path to your chromedriver
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined});")
+    return driver
+
+@app.route('/')
+def hello_world():
+    driver = driversetup()
+    try:
+        driver.get('https://www.google.com')
+        current_url = driver.current_url
+    except Exception as e:
+        current_url = f"An error occurred: {e}"
+    finally:
+        driver.quit()
+    
+    return f'{current_url} - Hello from Koyeb'
+
+if __name__ == "__main__":
     app.run()
+    
