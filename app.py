c03c48273084bcea9687a9201de4bedfa2bb2e9b
shibob53 <100147886+shibob53@users.noreply.github.com>
2024-06-13 18:36:58 +0300

    app.py

--- app.py
+++ app.py
@@ -1,95 +1,84 @@
-'''from flask import Flask
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#ere is the code I used to solve my issue:
 
-
-def driversetup():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # Run Selenium in headless mode
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("lang=en")
-    options.add_argument("start-maximized")
-    options.add_argument("disable-infobars")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--incognito")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    driver = webdriver.Chrome(options=options)
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined});")
-    return driver
-    
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-  driver = driversetup()
-  
-  return driver.current_url + 'Hello from Koyeb'
- 
- 
-if __name__ == "__main__":
-    app.run()'''
-#from flask import Flask
-from flask import Flask, request, jsonify
+from flask import Flask,request
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 import os
-import logging
-
+from time import sleep
+#import mido
+from threading import Thread
+import json
 app = Flask(__name__)
 
-# إعداد تسجيل الرسائل
-logging.basicConfig(level=logging.INFO)
-
+# List to store pressed keysnotes_pressed.
+#notes_pressed ={}
 def driversetup():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')  # Run Selenium in headless mode
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("lang=en")
-    options.add_argument("start-maximized")
-    options.add_argument("disable-infobars")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--incognito")
-    options.add_argument("--disable-blink-features=AutomationControlled")
+    #options.add_argument('--disable-dev-shm-usage')
+    #options.add_argument("lang=en")
+    #options.add_argument("start-maximized")
+    #options.add_argument("disable-infobars")
+    #options.add_argument("--disable-extensions")
+    #options.add_argument("--incognito")
+    #options.add_argument("--disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome(options=options)
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined});")
+    #driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined});")
     return driver
-Ldriver ={} #driversetup () 
-@app.route('/', methods=["POST"])
-def hello_world():
-    logging.info("Received request at root endpoint")
-    global Ldriver
+
+driver = driversetup()
+
+@app.route('/getuser', methods=['GET'])
+def getuser():
+  #data = request.get_json()
+  #s=data['s']
+  return driver.current_url
+
+"""
+@app.route('/get_notes', methods=['GET'])
+def return_pressed_notes():
+    return json.dumps(list(notes_pressed.keys()))
+@app.route('/get_note', methods=['POST'])
+def return_pressed_note():
     data = request.get_json()
     s = data['s'] 
-    u = data['u'] 
-    driver = driversetup()
-    try:
-        driver.get(u)
-        current_url = driver.current_url
-        Ldriver[s] =driver
-        logging.info(f"Current URL: {current_url}")
-    except Exception as e:
-        current_url = f"An error occurred: {e}"
-        logging.error(current_url)
+    p = Thread(target=get_next_note, args=(notes_pressed,s,))
+    p.start()
+    return "json.dumps(notes_pressed)"
+# Funnction to translate midi key numbers to note letters
+#def translate_key(key_num):
+    
+
+# Function that returns recently played note
+def get_next_note(notes_pressed,s):
+    # Open port to listen for note presses
+    for i in range(s):
+      sleep(3)
+      notes_pressed[i]= driversetup()
+      print("#########")
+      print(i)
+# Run main program
+"""
+@app.route('/user', methods=['GET'])
+def user():
+  #data = request.get_json()
+  #s=data['s']
+  try:
+    driver.get("https://sakani.sa/app/authentication/login")
+    current_url = driver.current_url
+    print(f"Current URL: {current_url}")
+  except Exception as e:
+    current_url = f"An error occurred: {e}"
+    print(current_url)
     #finally:
         #driver.quit()
     
-    return f'{current_url} - Hello from Render'
-@app.route('/hello', methods=["POST"])
-def hello():
-    data = request.get_json()
-    s = data ['s'] 
-    global Ldriver #= driversetup()
-    return Ldriver[s].current_url + ' Hello'
- 
-if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))
-    #logging.info(f"Starting server on port {port}")
+  return f'{current_url} - Hello from Render'
+if __name__ == '__main__':
+    # NEW CODE
+    #p = Thread(target=get_next_note, args=(notes_pressed,))
+    #p.start()
     app.run(debug=True)
-
-
-
-    
+    #app.run(host='0.0.0.0', use_reloader=False)
+    #p.join()&w
